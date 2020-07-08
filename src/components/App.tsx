2a5f17643ec9e044f14ae6e961e107f1f5dda343
Thibault Goehringer <tgoehringer@gmail.com>
2020-07-08 09:50:05 +0200

    Add basic files

---
+++ src/components/App.tsx
@@ -0,0 +1,20 @@
+import { h, Component } from 'preact';
+
+import './App.css';
+
+class App extends Component {
+
+  constructor(props: {}) {
+    super(props);
+	}
+
+	render () {
+		return (
+      <div id="app">
+      </div>
+    );
+	}
+
+}
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,48 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  resolve: {
+    extensions: [".js", ".jsx", ".ts", ".tsx"]
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts(x?)$/,
+        exclude: /node_modules/,
+        use: [{ loader: 'ts-loader' }]
+      },
+      {
+        enforce: "pre",
+        test: /\.js$/,
+        loader: "source-map-loader"
+      },
+      {
+        test: /\.css$/,
+        use: [{ loader: MiniCssExtractPlugin.loader }, 'css-loader']
+      },
+      {
+        test: /\.(woff(2)?|ttf|eot|svg|png|jpg|svg)$/,
+        use: [{ 
+          loader: 'file-loader',
+          options: {
+            name: '[name].[ext]',
+            outputPath: 'assets/',
+          }
+         }]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: __dirname + '/src/index.html',
+      title: 's-u-n'
+    }),
+    new MiniCssExtractPlugin()
+  ],
+  devServer: {
+    contentBase: __dirname + '/public',
+    historyApiFallback: true,
+    publicPath: '/',
+  }
+};
