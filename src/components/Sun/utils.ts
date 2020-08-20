d03002b3b2a5da7585c1616906201d66366a1441
Thibault Goehringer <tgoehringer@gmail.com>
2020-08-20 14:31:43 +0200

    refractored Sun utils a bit

--- src/components/Sun/utils.ts
+++ src/components/Sun/utils.ts
@@ -1,31 +1,31 @@
-export type Point = { x: number; y: number };
-export type Line = { start: Point; end: Point };
+export type Vector = { x: number; y: number };
+export type Line = { start: Vector; end: Vector };
 export type Size = { height: number; width: number };
 
-export const getPointOnCircle = (
-  center: Point,
-  radius: number,
-  angle: number
-): Point => ({
-  x: center.x + radius * Math.cos(angle),
-  y: center.y + radius * Math.sin(angle),
-});
+export const rotateVector = (vector: Vector, angle: number): Vector => {
+  const angleCos = Math.cos(angle);
+  const angleSin = Math.sin(angle);
+  return {
+    x: vector.x * angleCos - vector.y * angleSin,
+    y: vector.x * angleSin + vector.y * angleCos,
+  };
+};
 
 export const getTangent = (
-  center: Point,
+  center: Vector,
   radius: number,
   tangentLength: number,
   tangentAngle: number
 ): Line => {
-  const start = getPointOnCircle(center, radius, tangentAngle);
-  const hypot = Math.hypot(radius, tangentLength);
-  const angle = tangentAngle - Math.acos(radius / hypot);
-  const end = getPointOnCircle(center, hypot, angle);
+  const radiusVector = rotateVector({ x: radius, y: 0 }, tangentAngle);
+  const tangentVector = rotateVector({ x: 0, y: tangentLength }, tangentAngle);
+  const start = { x: center.x + radiusVector.x, y: center.y + radiusVector.y };
+  const end = { x: start.x + tangentVector.x, y: start.y + tangentVector.y };
   return { start, end };
 };
 
 export const getTangents = (
-  center: Point,
+  center: Vector,
   radius: number,
   tangentLength: number,
   nbTangents: number,
@@ -35,6 +35,7 @@ export const getTangents = (
   const angle = (Math.PI * 2) / nbTangents;
   // not using array mapping to increase performances
   const tangents = [];
+
   for (let i = 0; i < nbTangents; i++) {
     const tangent = getTangent(
       center,
