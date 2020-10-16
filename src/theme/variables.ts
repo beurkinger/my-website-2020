26e9bbd1f0a2b8d20cc1ff59775005f72087c7de
Thibault Goehringer <tgoehringer@gmail.com>
2020-10-16 11:38:29 +0200

    Test new colors

--- src/theme/variables.ts
+++ src/theme/variables.ts
@@ -1,10 +1,5 @@
 export default {
   customProperties: {
-    '--color-main-1': '#0000FF',
-    // '--color-main-1': '#3eb189',
-    // '--color-main-1': '#00b3a4',
-    '--color-main-2': '#FFEFEF',
-    '--color-black': '#FFEFEF',
     '--color-white': '#FFFFFF',
   },
 };
